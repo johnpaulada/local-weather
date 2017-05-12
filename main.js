425aa3eef94cf6892fef56efcd028c33cee162cd
John Paul Ada <johnpaulada@Elie-Mini.local.gap>
2017-05-12 10:30:46 +0800

    Abstracted the buildUrl code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+const {buildURL} = ParamHelpers
 const API_KEY   = 'b2709083cba938ec2aba09a017e2285d'
 const API_URL   = 'http://api.openweathermap.org/data/2.5/weather'
-
 const UNIT_SYSTEM = {
   METRIC: 'metric',
   IMPERIAL: 'imperial'
@@ -22,47 +22,13 @@ function setCoordinates(callback) {
   );
 }
 
-function decorateParam(url, key, value) {
-  const separator = url.includes('?') ? '&' : '?'
-
-  return `${url}${separator}${key}=${value}`
-}
-
-function makeParameterAdder(param) {
-  return function(url, value) {
-    return decorateParam(url, param, value)
-  }
-}
-
-/**
- * Builds a valid OpenWeatherMap API URL.
- *
- * @param {string} url The original OpenWeatherMap API URL,
- * @param {Object} coords The coordinates of
- *    the place where the weather should be taken.
- * @param {string} unitSystem The unit system of the measurements,
- *     e.g. Metric or Imperial.
- * @param {string} key The OpenWeatherMap API key.
- * @return {string} A valid OpenWeatherMap URL.
- */
-function buildOpenWeatherURL(url, coords, unitSystem, key) {
-  const addLatitudeParam = makeParameterAdder('lat')
-  const addLongitudeParam = makeParameterAdder('lon')
-  const addUnitSystemParam = makeParameterAdder('units')
-  const addApiKeyParam = makeParameterAdder('APPID')
-
-  const paramsList = [
-    [addLatitudeParam, coords.latitude],
-    [addLongitudeParam, coords.longitude],
-    [addUnitSystemParam, unitSystem],
-    [addApiKeyParam, key],
-  ]
-
-  return paramsList.reduce((url, param) => param[0](url, param[1]), url)
-}
-
-function fetchWeather(coords) {
-  const url = buildOpenWeatherURL(API_URL, coords, UNIT_SYSTEM.METRIC, API_KEY)
+function fetchWeather({latitude, longitude}) {
+  const url = buildURL(API_URL, {
+    'lat': latitude,
+    'lon': longitude,
+    'units': UNIT_SYSTEM.METRIC,
+    'APPID': API_KEY
+  })
 
   // TODO: Add loader
   console.log('Loading...')
@@ -75,7 +41,6 @@ function fetchWeather(coords) {
       console.log('Weather loaded.')
 
       // TODO: Display Data
-
       console.log(data)
     })
 }
